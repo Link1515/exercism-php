5839778736271a1c9209cac34a39f256bb8cca06
Link <h26281660@gmail.com>
2023-06-15 19:50:42 +0800

    collatz conjecture

---
+++ collatz-conjecture/CollatzConjecture.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+function steps(int $number): int
+{
+    if ($number <= 0) {
+        throw new InvalidArgumentException('Only positive numbers are allowed');
+    }
+
+    $count = 0;
+
+    while ($number !== 1) {
+        $count++;
+
+        if ($number % 2 === 0) {
+            $number /= 2;
+        } else {
+            $number = $number * 3 + 1;
+        }
+    }
+
+    return $count;
+}
